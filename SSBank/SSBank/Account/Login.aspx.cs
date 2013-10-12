dc014f0e9be992b56b8bb0cc596329f379e753cf
James Romo <jamestromo@gmail.com>
2013-10-11 18:37:37 -0700

    Added test DB actions

--- SSBank/SSBank/Account/Login.aspx.cs
+++ SSBank/SSBank/Account/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using SSBank.Modules;
 
 namespace SSBank.Account
 {
@@ -18,6 +19,24 @@ namespace SSBank.Account
             if (!String.IsNullOrEmpty(returnUrl))
             {
                 RegisterHyperLink.NavigateUrl += "?ReturnUrl=" + returnUrl;
+            }
+
+            // James Test DB Actions
+            using (var db = new BankingContext()) {
+                var question = new Question { question1 = "What is your mother's maiden name?" };
+                db.Questions.Add(question);
+                db.SaveChanges();
+
+                var query = from b in db.Questions
+                            orderby b.question1
+                            select b;
+
+                String databaseString = "All Questions in the database:";
+                foreach (var item in query)
+                {
+                    databaseString = databaseString + item.question1 + ", ";
+                }
+                //Response.Write(databaseString);
             }
         }
     }

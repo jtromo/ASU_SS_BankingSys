0e531af56467061eb546821c5f2080bc2b05750a
ushakanth_kvp <Chelsea10!>
2013-10-13 21:33:07 -0700

    no message

---
+++ SoftSec_BankingApp_Se7en/SoftSec_BankingApp_Se7en/AuthorizeUser.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SoftSec_BankingApp_Se7en
+{
+    public partial class AuthorizeUser : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SiteKey.ImageUrl = "./Images/SiteKeys/SiteKey1.jpg";
+        }
+
+        protected void Btn_SignIn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Btn_ForgotPassword_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("ForgotPassword.aspx");
+        }
+    }
+}
